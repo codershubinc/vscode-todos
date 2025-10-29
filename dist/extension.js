e3a75ce00feb3f087181edcc6184cc69968a94a4
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-10-29 14:41:04 +0530

    Replace minified dist bundle with readable webpack output; add view icon and placeholders
    - Replace dist/extension.js with expanded webpack output (adds webview HTML, enableScripts, localResourceRoots and resolve notification)
    - Update extension.js.map to match new bundle
    - Add "icon" to explorer Todos view in package.json
    - Add placeholder t.css and t.html files

--- dist/extension.js
+++ dist/extension.js
@@ -1,1 +1,164 @@
-(()=>{"use strict";var t={265:function(t,e,o){var n,i=this&&this.__createBinding||(Object.create?function(t,e,o,n){void 0===n&&(n=o);var i=Object.getOwnPropertyDescriptor(e,o);i&&!("get"in i?!e.__esModule:i.writable||i.configurable)||(i={enumerable:!0,get:function(){return e[o]}}),Object.defineProperty(t,n,i)}:function(t,e,o,n){void 0===n&&(n=o),t[n]=e[o]}),r=this&&this.__setModuleDefault||(Object.create?function(t,e){Object.defineProperty(t,"default",{enumerable:!0,value:e})}:function(t,e){t.default=e}),s=this&&this.__importStar||(n=function(t){return n=Object.getOwnPropertyNames||function(t){var e=[];for(var o in t)Object.prototype.hasOwnProperty.call(t,o)&&(e[e.length]=o);return e},n(t)},function(t){if(t&&t.__esModule)return t;var e={};if(null!=t)for(var o=n(t),s=0;s<o.length;s++)"default"!==o[s]&&i(e,t,o[s]);return r(e,t),e});Object.defineProperty(e,"__esModule",{value:!0}),e.activate=function(t){console.log('Congratulations, your extension "codershubinc" is now active!'),console.log("Testing the extension +++++++++++++++++++++++++++++++++++++++++++++++++");const e=new a(t.extensionUri);t.subscriptions.push(l.window.registerWebviewViewProvider(a.viewType,e));const o=l.commands.registerCommand("codershubinc.helloWorld",()=>{l.window.showInformationMessage("Hello World from vs todo!")}),n=l.commands.registerCommand("vs-todo.showTodosWebview",()=>{});t.subscriptions.push(o),t.subscriptions.push(n)},e.deactivate=function(){};const l=s(o(398));class a{_extensionUri;static viewType="todosView";constructor(t){this._extensionUri=t,console.log("Registering the webview provider +++++++++++++++++++++++++++++++++++++++++++++++++")}resolveWebviewView(t,e,o){console.log("Resolving the webview view +++++++++++++++++++++++++++++++++++++++++++++++++"),l.window.showInformationMessage("Todos view resolved (resolveWebviewView called)"),t.webview.options={enableScripts:!0,localResourceRoots:[this._extensionUri]},t.webview.html=this._getHtmlForWebview()}_getHtmlForWebview(){return'<!DOCTYPE html>\n\t\t\t<html lang="en">\n\t\t\t<head>\n\t\t\t\t<meta charset="UTF-8">\n\t\t\t\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t\t\t\t<title>Todos</title>\n\t\t\t\t<style>\n\t\t\t\t\tbody {\n\t\t\t\t\t\tpadding: 10px;\n\t\t\t\t\t\tfont-family: var(--vscode-font-family);\n\t\t\t\t\t\tcolor: var(--vscode-foreground);\n\t\t\t\t\t}\n\t\t\t\t\th1 {\n\t\t\t\t\t\tcolor: var(--vscode-foreground);\n\t\t\t\t\t\tmargin: 0 0 10px 0;\n\t\t\t\t\t}\n\t\t\t\t</style>\n\t\t\t</head>\n\t\t\t<body>\n\t\t\t\t<h1>todos</h1>\n\t\t\t\t<p>Your todos will appear here...</p>\n\t\t\t</body>\n\t\t\t</html>'}}},398:t=>{t.exports=require("vscode")}},e={},o=function o(n){var i=e[n];if(void 0!==i)return i.exports;var r=e[n]={exports:{}};return t[n].call(r.exports,r,r.exports,o),r.exports}(265);module.exports=o})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.activate = activate;
+exports.deactivate = deactivate;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = __importStar(__webpack_require__(1));
+class TodosViewProvider {
+    _extensionUri;
+    static viewType = 'todosView';
+    constructor(_extensionUri) {
+        this._extensionUri = _extensionUri;
+        console.log("Registering the webview provider +++++++++++++++++++++++++++++++++++++++++++++++++");
+    }
+    resolveWebviewView(webviewView, context, _token) {
+        console.log("Resolving the webview view +++++++++++++++++++++++++++++++++++++++++++++++++");
+        // Show a visible notification so it's obvious when the view is resolved.
+        // This is helpful for debugging — the Extension Host log may be missed.
+        vscode.window.showInformationMessage('Todos view resolved (resolveWebviewView called)');
+        webviewView.webview.options = {
+            enableScripts: true,
+            localResourceRoots: [
+                this._extensionUri
+            ]
+        };
+        webviewView.webview.html = this._getHtmlForWebview();
+    }
+    _getHtmlForWebview() {
+        return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Todos</title>
+				<style>
+					body {
+						padding: 10px;
+						font-family: var(--vscode-font-family);
+						color: var(--vscode-foreground);
+					}
+					h1 {
+						color: var(--vscode-foreground);
+						margin: 0 0 10px 0;
+					}
+				</style>
+			</head>
+			<body>
+				<h1>todos</h1>
+				<p>Your todos will appear here...</p>
+			</body>
+			</html>`;
+    }
+}
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "codershubinc" is now active!');
+    console.log("Testing the extension +++++++++++++++++++++++++++++++++++++++++++++++++");
+    // Register the webview provider for the Explorer panel
+    const provider = new TodosViewProvider(context.extensionUri);
+    // console.log("Registering the webview provider +++++++++++++++++++++++++++++++++++++++++++++++++");
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider(TodosViewProvider.viewType, provider));
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    const disposable = vscode.commands.registerCommand('codershubinc.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World from vs todo!');
+    });
+    const disposable2 = vscode.commands.registerCommand('vs-todo.showTodosWebview', () => {
+    });
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable2);
+}
+// This method is called when your extension is deactivated
+function deactivate() { }
+
+
+/***/ }),
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("vscode");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__(0);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=extension.js.map
